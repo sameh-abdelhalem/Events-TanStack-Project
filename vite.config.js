d2c11f9faf8e15183053349bd4ae5c1ce53d3640
Sameh <sameh.abdelhaleem95@gmail.com>
2024-02-28 12:10:31 +0200

    add deploy

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/Events-TanStack-Project/",
   plugins: [react()],
+  base: "/Events-TanStack-Project/",
 });
