fb1b43f4d9d2c9606403da0c5e8cf88fa134df55
Sameh <sameh.abdelhaleem95@gmail.com>
2024-02-28 13:26:12 +0200

    another deployment attempt

--- src/App.jsx
+++ src/App.jsx
@@ -11,36 +11,33 @@ import EditEvent from "./components/Events/EditEvent.jsx";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { queryClient } from "./util/http.js";
 
-const router = createBrowserRouter(
-  [
-    {
-      path: "/",
-      element: <Navigate to="/events" />,
-    },
-    {
-      path: "/events",
-      element: <Events />,
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Navigate to="/events" />,
+  },
+  {
+    path: "/events",
+    element: <Events />,
 
-      children: [
-        {
-          path: "/events/new",
-          element: <NewEvent />,
-        },
-      ],
-    },
-    {
-      path: "/events/:id",
-      element: <EventDetails />,
-      children: [
-        {
-          path: "/events/:id/edit",
-          element: <EditEvent />,
-        },
-      ],
-    },
-  ],
-  { basename: "/Events-TanStack-Project/" }
-);
+    children: [
+      {
+        path: "/events/new",
+        element: <NewEvent />,
+      },
+    ],
+  },
+  {
+    path: "/events/:id",
+    element: <EventDetails />,
+    children: [
+      {
+        path: "/events/:id/edit",
+        element: <EditEvent />,
+      },
+    ],
+  },
+]);
 
 function App() {
   return (
