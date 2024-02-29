5d3a699e364ea8e79fdc77a54eb0ce9022f62615
Sameh <sameh.abdelhaleem95@gmail.com>
2024-02-29 11:37:07 +0200

    remove UI bug

--- src/App.jsx
+++ src/App.jsx
@@ -11,38 +11,41 @@ import EditEvent from "./components/Events/EditEvent.jsx";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { queryClient } from "./util/http.js";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Navigate to="/events" />,
-  },
-  {
-    path: "/events",
-    element: <Events />,
+const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <Navigate to="/events" />,
+    },
+    {
+      path: "/events",
+      element: <Events />,
 
-    children: [
-      {
-        path: "/events/new",
-        element: <NewEvent />,
-      },
-    ],
-  },
-  {
-    path: "/events/:id",
-    element: <EventDetails />,
-    children: [
-      {
-        path: "/events/:id/edit",
-        element: <EditEvent />,
-      },
-    ],
-  },
-]);
+      children: [
+        {
+          path: "/events/new",
+          element: <NewEvent />,
+        },
+      ],
+    },
+    {
+      path: "/events/:id",
+      element: <EventDetails />,
+      children: [
+        {
+          path: "/events/:id/edit",
+          element: <EditEvent />,
+        },
+      ],
+    },
+  ],
+  { basename: "/Events-TanStack-Project" }
+);
 
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
-      <RouterProvider router={router} />;
+      <RouterProvider router={router} />
     </QueryClientProvider>
   );
 }
